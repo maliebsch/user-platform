ad7fbe8d2897fa95c3ea37f346159c309a0981d1
maliebsch <38839673+maliebsch@users.noreply.github.com>
2020-09-16 10:11:42 +0100

    project set up, app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Background from './images/background.jpg';
+import styles from './styles/App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div
+          className={styles.app}
+          data-test="component-app"
+          style={{ backgroundImage: `url("${Background}")` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Switch>
+            {/* <Route exact path="/" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/sport" component={SportSearch} />
+            <Route exact path="/photos" component={PhotoUpload} />
+            <Route exact path="/news" component={News} />
+            <Route exact path="/news/latest" component={NewsArticle} /> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders  without throwing error', () => {
+  const wrapper = shallow(<App />);
+  const appComp = wrapper.find("[data-test='component-app']");
+  expect(appComp.length).toBe(1);
+});
+
+test('renders background image', () => {
+  const wrapper = shallow(<App />);
+  const appCompStyle = wrapper.find("[data-test='component-app']").get(0).props
+    .style;
+  expect(appCompStyle).toHaveProperty('backgroundImage');
 });
